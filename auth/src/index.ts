0c970fabbefdc9378ff87ddcdba4a92281c56f9c
Andy <andy.li@cohesity.com>
2021-06-28 12:54:57 -0400

    done error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,19 +1,27 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
-import { currentuserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorhandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
-app.use(currentuserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorhandler);
 
 app.listen(3000, () => {
-  console.log("listen to port 3000!!!!");
+  console.log("listen to port 3000!");
 });
